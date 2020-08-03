5b10aac72f32af80d315ec5188c78ad307369520
AsadHasan <asadhasan180@hotmail.com>
2020-08-03 18:37:42 +0100

    initial commit

---
+++ cypress/integration/linuxacademy.spec.js
@@ -0,0 +1,22 @@
+describe("Linux academy", () => {
+  it("Should be accessible from the internet", () => {
+    cy.visit("https://linuxacademy.com/");
+  });
+
+  it("Should contain 'Hands-On Labs'", () => {
+    cy.contains("Hands-On Labs");
+  });
+
+  it("Should show 'Popular Hands-On Labs'", () => {
+    cy.get("button.toggle--nav").click();
+    cy.contains("Popular Hands-On Labs");
+  });
+
+  it("Should have option to 'Learn by platform'", () => {
+    cy.contains("Learn by platform");
+  });
+
+  it("Should have option to 'Learn by skill'", () => {
+    cy.contains("Learn by skill");
+  });
+});
