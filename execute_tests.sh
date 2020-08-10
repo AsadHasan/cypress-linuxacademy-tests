8211872c5260fda63622d77801d0c0147615dd70
AsadHasan <asadhasan180@hotmail.com>
2020-08-10 11:58:35 +0100

    overwrote docker entrypoint, to install cypress-axe plugin also, to allow running tests with it

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,2 +1,5 @@
 docker run -it -v $PWD:/e2e -w /e2e \
-  -e CYPRESS_RECORD_KEY cypress/included:4.12.0 --record
+  -e CYPRESS_RECORD_KEY \
+  --entrypoint="" \
+  cypress/included:4.12.0 \
+	/bin/bash -c 'npm install cypress-axe && cypress run --spec "cypress/integration/**/*" --record'
